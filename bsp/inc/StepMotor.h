0e96bc2e714bbf4c79d98d34dbbe234de488b5fb
loong <3516197926@qq.com>
2025-01-11 22:40:02 +0800

    第一次提交

---
+++ bsp/inc/StepMotor.h
@@ -0,0 +1,176 @@
+/**
+ * @file StepMotor.h
+ * @author Elaina (1463967532@qq.com)
+ * @brief 使用方法:
+ * 1.先初始化一个步进电机实例,并且设置好参数
+ * Motor = StepMotor_t(&htim2, TIM_CHANNEL_2, GPIOB, GPIO_PIN_4);
+ * Motor.setParam(1.8, 8, 10, 100);
+ * 2.注册回调函数
+ * void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim)
+ * {Motor.dmaCallBack();}
+ * 3.在定时器中断,主循环或者rtos任务重复调用update函数,下面是主循环的例子
+ * while(1)
+ * {
+ * Motor.update(1);
+ * Hal_Delay(5);
+ * }
+ * @version 0.1
+ * @date 2025-01-04
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#ifndef __STEPMOTOR_H
+#define __STEPMOTOR_H
+#include "main.h"
+#include "stdint.h"
+#include "string.h"
+#define BUFFER_SIZE 500  // 缓冲区大小越大占用cpu时间越少
+#define ITERATIONS 10    // 缓起缓停迭代次数
+#define ITERATIONPULSE 8 // 缓起缓停迭代角度
+enum StepMotorState
+{
+    BUSY,          // 正在输出脉冲
+    IDLE,          // 空闲
+    WaitIteration, // 等待迭代
+    Start,         // 启动
+};
+/**
+ * @brief 步进调用定时器默认的基频为1MHz
+ * 分辨率为100
+ *
+ */
+class StepMotor_t
+{
+public:
+    // 给一定频率一定数量的脉冲
+    StepMotor_t()
+    {
+    }
+    /**
+     * @brief 新建一个步进电机实例
+     *
+     * @param tim pwm对应的定时器
+     * @param channel pwm对应的通道
+     * @param ph_port 步进电机的相位端口
+     * @param ph_pin 步进电机的相位引脚
+     */
+    StepMotor_t(TIM_HandleTypeDef *tim, uint32_t channel, GPIO_TypeDef *ph_port, uint16_t ph_pin)
+    {
+        _tim = tim;
+        _ph_port = ph_port;
+        _ph_pin = ph_pin;
+        _channel = channel;
+        __HAL_TIM_SetAutoreload(_tim, _resolution - 1);
+    }
+    /**
+     * @brief 设置步进电机的参数
+     *
+     * @param StepAngle 步进角
+     * @param Subdivision 细分
+     * @param target_iteration_num 缓起迭代次数
+     * @param itertion_pulse 缓起迭代脉冲数
+     */
+    void setParam(float StepAngle, uint8_t Subdivision, uint16_t target_iteration_num, uint16_t itertion_pulse)
+    {
+        _StepAngle = StepAngle;
+        _Subdivision = Subdivision;
+        _itertration_num = target_iteration_num;
+        _iteration_pulse = itertion_pulse;
+    }
+    /**
+     * @brief 是否空闲
+     *
+     * @return true
+     * @return false
+     */
+    bool isBusy(void)
+    {
+        return !(_state == IDLE);
+    }
+    void update(uint16_t dt);
+    /**
+     * @brief 缓启缓停转动一定脉冲数
+     *
+     * @param rpm 转速
+     * @param pulse 脉冲数
+     * @param target_iteration_num 缓起迭代次数
+     * @param itertion_pulse 缓起迭代脉冲数 越大迭代时间越长
+     */
+    void giveRPMPulseSoft(float rpm, uint32_t pulse, uint16_t target_iteration_num = 0, uint16_t itertion_pulse = 0);
+    /**
+     * @brief 按照一定的转速给一定数量的脉冲
+     *
+     * @param rpm 转数
+     * @param pulse 脉冲数
+     */
+    void giveRPMPulse(float rpm, uint32_t pulse);
+    /**
+     * @brief 按照一定转速转一个角度
+     *
+     * @param rpm 转速
+     * @param angle 角度
+     */
+    void giveRPMAngle(float rpm, float angle);
+    /**
+     * @brief dma回调
+     *
+     */
+    void dmaCallBack(TIM_HandleTypeDef *htim);
+    /**
+     * @brief 将角度转换为脉冲数
+     *
+     * @param angle 角度
+     * @return uint32_t 脉冲数
+     */
+    uint32_t angleToPulse(float angle);
+
+public:
+    /**
+     * @brief 给一定频率一定数量的脉冲,底层通过多次调用giveOncePulse来实现,通过回调来执行下一次调用
+     *
+     * @param pulse 脉冲数量
+     * @param freq 脉冲的频率
+     */
+    void givePulse(uint32_t pulse, uint32_t freq = 20000);
+    /**
+     * @brief 给一串脉冲,脉冲的频率通过基频的间隔来实现
+     *
+     * @param pulse 脉冲数量
+     * @param freq 脉冲的基频默认为20KHz
+     * @param clear_buffer 是否清空缓冲区
+     */
+    void giveOncePulse(uint32_t pulse, uint32_t freq = 20000, bool clear_buffer = true);
+    /**
+     * @brief Get the Iteration Data object 获取迭代数据
+     *
+     * @param rpm_in 迭代的转速
+     * @param pulse_in 迭代的脉冲数
+     */
+    void getIterationData(float &rpm_in, uint32_t &pulse_in);
+    /*缓启缓停相关*/
+    uint16_t _iteration = 0;                    // 当前迭代次数
+    uint32_t _soft_target_pulse = 0;            // 缓启缓停的目标脉冲数
+    float _soft_target_rpm = 0;                 // 缓启缓停的目标最大转速
+    uint16_t _itertration_num = ITERATIONS;     // 缓启缓停的迭代次数
+    uint16_t _iteration_pulse = ITERATIONPULSE; // 缓启缓停的每圈转速对应的脉冲数
+    /*步进电机参数相关*/
+    float _StepAngle = 1.8;   // 步进角
+    uint8_t _Subdivision = 8; // 细分
+    /*给一定数量脉冲实现相关*/
+    uint16_t _pulse_mod;           // 取余的脉冲数
+    uint32_t _target_pulse = 0;    // 给一串脉冲的目标脉冲数
+    uint32_t _target_freq = 20000; // 给一串脉冲的目标频率
+    uint16_t _target_number = 0;   // 给一串脉冲的目标次数
+    StepMotorState _state = IDLE;
+    /*硬件外设相关*/
+    TIM_HandleTypeDef *_tim;
+    GPIO_TypeDef *_ph_port;
+    uint32_t _channel;
+    uint16_t _ph_pin;                         // 预分频
+    uint32_t _clock_base_frequency = 1000000; // 时钟频率经过预分频后的频率
+    uint8_t _resolution = 10;                 // 分辨率
+    uint32_t _buffer[BUFFER_SIZE];
+};
+
+#endif
---
+++ bsp/src/StepperMotor.cpp
@@ -0,0 +1,203 @@
+/**
+ * @file StepperMotor.cpp
+ * @author Elaina (1463967532@qq.com)
+ * @brief
+ * @version 0.1
+ * @date 2025-01-04
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#include "StepMotor.h"
+#define abs(x) ((x) > 0 ? (x) : -(x))
+void StepMotor_t::update(uint16_t dt)
+{
+    // 1ms的时间间隔
+    switch (_state)
+    {
+    case BUSY:
+        return;
+        break;
+    case IDLE:
+        return;
+        break;
+    case Start:
+        _state = WaitIteration;
+        break;
+    case WaitIteration:
+        if (_iteration == 2 * _itertration_num)
+        {
+            _iteration = 0;
+            _state = IDLE;
+        }
+        else
+        {
+            float temp_rpm;
+            uint32_t temp_pulse;
+            getIterationData(temp_rpm, temp_pulse);
+            giveRPMPulse(temp_rpm, temp_pulse);
+            _iteration++;
+        }
+        break;
+    default:
+        break;
+    }
+}
+void StepMotor_t::giveRPMPulseSoft(float rpm, uint32_t pulse, uint16_t target_iteration_num, uint16_t itertion_pulse)
+{
+    if (target_iteration_num != 0 && itertion_pulse != 0)
+    {
+        _itertration_num = target_iteration_num;
+        _iteration_pulse = itertion_pulse;
+    }
+    _soft_target_pulse = pulse;
+    _soft_target_rpm = rpm;
+
+    if (_state != IDLE || pulse == 0||abs(rpm)<0.001f)
+    {
+        return;
+    }
+    // 算出剩余的脉冲数量
+    else
+    {
+        _state = Start;
+    }
+}
+void StepMotor_t::giveRPMPulse(float rpm, uint32_t pulse)
+{
+    uint32_t freq = _Subdivision * abs(rpm) * (360 / _StepAngle);
+    if (pulse == 0)
+    {
+        return;
+    }
+    // 脉冲数量
+    if (rpm > 0)
+    {
+        HAL_GPIO_WritePin(_ph_port, _ph_pin, GPIO_PIN_SET);
+    }
+    else
+    {
+        HAL_GPIO_WritePin(_ph_port, _ph_pin, GPIO_PIN_RESET);
+    }
+    givePulse(pulse, freq);
+}
+void StepMotor_t::giveRPMAngle(float rpm, float angle)
+{
+    // 脉冲频率
+    uint32_t freq = _Subdivision * abs(rpm) * (360 / _StepAngle);
+    // 脉冲数量
+    uint32_t pulse = _Subdivision * angle / _StepAngle;
+    if (rpm > 0)
+    {
+        HAL_GPIO_WritePin(_ph_port, _ph_pin, GPIO_PIN_SET);
+    }
+    else
+    {
+        HAL_GPIO_WritePin(_ph_port, _ph_pin, GPIO_PIN_RESET);
+    }
+    givePulse(pulse, freq);
+}
+void StepMotor_t::givePulse(uint32_t pulse, uint32_t freq)
+{
+    // 先算出每隔几个频率发一次脉冲
+    uint32_t period = _clock_base_frequency / freq / _resolution;
+    uint8_t arr = _resolution / 2; // 占空比为50%
+    //  先清零缓冲区
+    uint32_t max_address = pulse * period;
+    // 如果不被整除情况下要加一次
+    // _target_number = max_address / BUFFER_SIZE + (((max_address % BUFFER_SIZE) == 0) ? 0 : 1);
+    // 每次循环的目标脉冲数,除了最后一次
+    _target_pulse = BUFFER_SIZE / period > pulse ? pulse : BUFFER_SIZE / period;
+    // 如果不被整除情况下要加一次
+    _target_number = pulse / _target_pulse + ((pulse % _target_pulse == 0) ? 0 : 1);
+    // 最后一次的脉冲数,考虑只有执行一次的情况
+    _pulse_mod = pulse - _target_pulse * (_target_number - 1);
+    if (_target_number == 1)
+    {
+        _pulse_mod = 0;
+    }
+    _target_freq = freq;
+    giveOncePulse(_target_pulse, _target_freq);
+    _state = BUSY;
+}
+void StepMotor_t::giveOncePulse(uint32_t pulse, uint32_t freq, bool clearbuffer)
+{
+    // 先算出每隔几个频率发一次脉冲
+    uint32_t period = _clock_base_frequency / freq / _resolution;
+    uint8_t ccr = _resolution / 2; // 占空比为50%
+    // 先清零缓冲区
+    uint16_t max_address = pulse * period;
+    if (max_address > BUFFER_SIZE)
+    {
+        return;
+    }
+    if (clearbuffer)
+    {
+        memset(_buffer, 0, BUFFER_SIZE);
+        for (uint16_t i = 0; i < pulse; i += 1)
+        {
+            uint16_t j = i * period;
+            _buffer[j] = ccr;
+        }
+    }
+
+    HAL_TIM_PWM_Start_DMA(_tim, _channel, (uint32_t *)_buffer, max_address);
+}
+void StepMotor_t::dmaCallBack(TIM_HandleTypeDef *htim)
+{
+    if (htim->Instance != _tim->Instance)
+    {
+        return;
+    }
+    HAL_TIM_PWM_Stop_DMA(_tim, _channel);
+    if (_target_number > 2)
+    {
+        giveOncePulse(_target_pulse, _target_freq, false);
+        _target_number--;
+    }
+    else if (_target_number == 2 && _pulse_mod > 0)
+    {
+        giveOncePulse(_pulse_mod, _target_freq);
+        _target_number--;
+    }
+    else
+    {
+        HAL_TIM_PWM_Stop_DMA(_tim, _channel);
+        // _state = IDLE;
+        _state = WaitIteration;
+    }
+}
+
+uint32_t StepMotor_t::angleToPulse(float angle)
+{
+    return angle / _StepAngle * _Subdivision;
+}
+
+void StepMotor_t::getIterationData(float &rpm_in, uint32_t &pulse_in)
+{
+    if (_iteration < _itertration_num)
+    {
+        rpm_in = _soft_target_rpm * _iteration / _itertration_num;
+        pulse_in = abs(rpm_in) * _iteration_pulse;
+        _soft_target_pulse -= 2 * pulse_in;
+        if (pulse_in < 0)
+        {
+            pulse_in = 0;
+        }
+    }
+    else if (_iteration == _itertration_num)
+    {
+        rpm_in = _soft_target_rpm;
+        pulse_in = _soft_target_pulse;
+    }
+    else if (_iteration <= 2 * _itertration_num)
+    {
+        rpm_in = _soft_target_rpm * (_itertration_num * 2 - _iteration) / _itertration_num;
+        pulse_in = abs(rpm_in) * _iteration_pulse;
+    }
+    else
+    {
+        rpm_in = 0;
+        pulse_in = 0;
+    }
+}
